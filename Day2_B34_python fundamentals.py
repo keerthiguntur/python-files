245edf988897d7df05ca9d9f38794302fc0702a1
keerthiguntur <62271856+keerthiguntur@users.noreply.github.com>
2021-01-13 22:59:40 +0530

    Add files via upload

---
+++ Day2_B34_python fundamentals.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first name = 'keerthi'
+print(first name)
+
+
+# In[2]:
+
+
+firstname = 'keerthi'
+print(firstname)
+
+
+# In[3]:
+
+
+first_name = 'keerthi'
+print(first_name)
+
+
+# In[4]:
+
+
+1name = 'guntur'
+print(1name)
+
+
+# In[5]:
+
+
+name1 = 'guntur'
+print(name1)
+
+
+# In[6]:
+
+
+name@ = 'python'
+print(name@)
+
+
+# In[7]:
+
+
+*name= 'python'
+print(*name)
+
+
+# In[8]:
+
+
+name1 = 'keerthi guntur'
+print(name1)
+
+
+# In[9]:
+
+
+name2 = "kiran"
+print(name2)
+
+
+# In[10]:
+
+
+name3= "'tillu'"
+print(name3)
+
+
+# In[ ]:
+
+
+
+
